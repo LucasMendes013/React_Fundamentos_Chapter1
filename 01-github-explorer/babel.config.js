16245c2895affddec9d47ea616fe19faee392ba0
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-07-27 09:04:32 -0300

    att

---
+++ 01-github-explorer/babel.config.js
@@ -0,0 +1,8 @@
+//O Babel auxilia na passagem do Javascript de versões anteriores para a dos novos navegadores
+
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ]
+}
